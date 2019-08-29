eb3cb883ba89a6d5b9bb79f2cca297930280d5e5
mustafakrms <karamesemustafa24@gmail.com>
2019-08-29 10:01:56 +0300

    Static Files added

--- MovieApp/Startup.cs
+++ MovieApp/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace MovieApp
 {
@@ -21,6 +23,12 @@ namespace MovieApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();//wwwroot => /css/style.css --- /img/1.jpg
+            app.UseStaticFiles(new StaticFileOptions {
+                FileProvider=new PhysicalFileProvider(Path.Combine
+                (Directory.GetCurrentDirectory(),"Dependencies")),
+                RequestPath="/npm"
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
