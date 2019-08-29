0f8960cb4fe6c162148cf6d05ba855e68a0227d2
mustafakrms <karamesemustafa24@gmail.com>
2019-08-29 16:27:22 +0300

    Adding Active Class

--- MovieApp/ViewCompanents/CategoryMenuViewComponent.cs
+++ MovieApp/ViewCompanents/CategoryMenuViewComponent.cs
@@ -11,6 +11,11 @@ namespace MovieApp.ViewCompanents
     {
         public IViewComponentResult Invoke()
         {
+            if (RouteData.Values["action"].ToString()=="Index")
+            {
+                ViewBag.SelectedCategory = RouteData?.Values["id"];
+            }
+            
             return View(CategoryRepository.Categories);
         }
     }
